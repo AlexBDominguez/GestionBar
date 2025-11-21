dabf3ab2f4013f0bd4f0a0bc4a892736aa87d007
AlexBDominguez <alexwirtzfp@gmail.com>
2025-11-21 18:02:26 +0100

    Initial commit - Gestión de Bar

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'view/home_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      theme: ThemeData(
+       
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+
+ 
---
+++ lib/model/pedido.dart
@@ -0,0 +1,13 @@
+class Pedido {
+
+  String nombreMesa = '';
+  int numeroProductos = 0;
+  double precioTotal = 0.0;
+
+  Pedido({
+    required this.nombreMesa,
+    required this.numeroProductos,
+    required this.precioTotal,
+  });
+
+}
